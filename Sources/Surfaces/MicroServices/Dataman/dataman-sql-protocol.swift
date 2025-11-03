d0b0816b8de1b703918f1c5b539863c00e8fb496
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-03 11:25:14 +0100

    include statics

--- Sources/Surfaces/MicroServices/Dataman/dataman-sql-protocol.swift
+++ Sources/Surfaces/MicroServices/Dataman/dataman-sql-protocol.swift
@@ -6,4 +6,9 @@ public protocol DatamanSQLBuilding: Sendable {
     func buildInsert(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
     func buildUpdate(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
     func buildDelete(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
+
+    static func buildSelect(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
+    static func buildInsert(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
+    static func buildUpdate(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
+    static func buildDelete(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
 }
