9ec2eb0d58d353d99c2b8f7403d1f12e9664d4e1
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-03 14:26:21 +0100

    preparedcontent for retroactive savings

--- Sources/Surfaces/MicroServices/Captcher/captcher-models/captcher-metrics/captcher-decision.swift
+++ Sources/Surfaces/MicroServices/Captcher/captcher-models/captcher-metrics/captcher-decision.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public enum CaptcherDecision: String, RawRepresentable, Sendable, Codable {
+public enum CaptcherDecision: String, RawRepresentable, PreparedContent {
     case pass
     case allowFlag
     case challenge
