0e4b78120e79a3d7ddc9a096e4b9fba564ea1b7d
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-03 11:28:26 +0100

    change arg labels, comment out non-statics

--- Sources/Surfaces/MicroServices/Dataman/dataman-sql-protocol.swift
+++ Sources/Surfaces/MicroServices/Dataman/dataman-sql-protocol.swift
@@ -2,13 +2,13 @@ import Foundation
 import Constructors
 
 public protocol DatamanSQLBuilding: Sendable {
-    func buildSelect(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
-    func buildInsert(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
-    func buildUpdate(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
-    func buildDelete(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
+    // func buildSelect(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
+    // func buildInsert(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
+    // func buildUpdate(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
+    // func buildDelete(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
 
-    static func buildSelect(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
-    static func buildInsert(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
-    static func buildUpdate(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
-    static func buildDelete(_ r: DatamanRequest) throws -> PSQL.RenderedSQL
+    static func buildSelect(from request: DatamanRequest) throws -> PSQL.RenderedSQL
+    static func buildInsert(from request: DatamanRequest) throws -> PSQL.RenderedSQL
+    static func buildUpdate(from request: DatamanRequest) throws -> PSQL.RenderedSQL
+    static func buildDelete(from request: DatamanRequest) throws -> PSQL.RenderedSQL
 }
