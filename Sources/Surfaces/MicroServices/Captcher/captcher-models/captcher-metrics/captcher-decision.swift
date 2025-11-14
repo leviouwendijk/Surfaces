202b2931e93dc5fac4aefb5208fdb0314a288dd2
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-14 23:05:52 +0100

    removal of deprecation

--- Sources/Surfaces/MicroServices/Captcher/captcher-models/captcher-metrics/captcher-decision.swift
+++ Sources/Surfaces/MicroServices/Captcher/captcher-models/captcher-metrics/captcher-decision.swift
@@ -1,7 +1,7 @@
 import Foundation
 import plate
 
-public enum CaptcherDecision: String, RawRepresentable, PreparableContent {
+public enum CaptcherDecision: String, RawRepresentable, Sendable, Codable {
     case pass
     case allowFlag
     case challenge
